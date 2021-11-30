3dee1bf2863c05ea2789f36e3b3c593e6e531beb
SanghyeonGo <ksh0614@krx.co.kr>
2021-11-30 14:32:21 +0900

    교집합 함수연습

---
+++ Function1.py
@@ -0,0 +1,28 @@
+#Function1.py
+#함수를 정의
+def setValue(newValue):
+    #함수 내부에서 초기화하면 지역변수(Local Variable)
+    x = newValue
+    print("함수내부:", x)
+
+
+#함수를 호출
+result = setValue(5)
+print(result)
+
+#다중의 값을 리턴
+def swap(x,y):
+    return y,x
+
+#호출
+result = swap(5,6)
+print(result)
+print( result[0], result[1] )
+
+#디버깅연습(교집합문자를 리턴)
+def union(prelist, postlist):
+    result = []
+    for x in prelist:
+        if x in postlist and x not in result:
+            result.append(x)
+    return result
